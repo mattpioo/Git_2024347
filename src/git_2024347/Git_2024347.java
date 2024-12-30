2d03d597d2e74b6061b81e822888d899ab8a8879
matheusFAG <2024347@student.cct.ie>
2024-12-30 23:10:55 +0000

    A simple print was added

--- src/git_2024347/Git_2024347.java
+++ src/git_2024347/Git_2024347.java
@@ -14,7 +14,8 @@ public class Git_2024347 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        System.out.println("Matheus Rodrigo Fagundes - 2024347");
     }
     
 }
