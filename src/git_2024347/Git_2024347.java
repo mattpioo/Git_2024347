8bea6c9817fd57fd99247284dee2fabfafc53b68
matheusFAG <matheusfagundes10029@gmail.com>
2025-02-01 13:26:54 -0300

    Last homework's code commitment (on strings)

--- src/git_2024347/Git_2024347.java
+++ src/git_2024347/Git_2024347.java
@@ -4,6 +4,8 @@
  */
 package git_2024347;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mathe
@@ -15,7 +17,31 @@ public class Git_2024347 {
      */
     public static void main(String[] args) {
 
-        System.out.println("Matheus Rodrigo Fagundes - 2024347");
-    }
+        Scanner myKB = new Scanner(System.in); // creates a Scanner
+        String name; // creates a variable (name)
+
+       
+        while (true) { // loops until the user enters a valid text with no numbers
+            System.out.print("Please enter your name: "); // prompts the user for input
+            name = myKB.nextLine(); // reads input from user and assigns it to variable (name)
+
+           
+            if (name.matches("[0-9]+")) {  // checks if the input contains any numbers
+                System.out.println("Your name must contain letters only. Please try again."); // outputs error message incase it does contains numbers
+            } else {
+                break; // exits loop if the input is vali d
+            }
+        }
+
+        // the next part of the project transforms the name
+     String upperCaseName = name.toUpperCase(); // converts the name to uppercase
+     String modifiedName = name.substring(1) + name.charAt(0); // moves the first letter to the end of the name. 
+
+        // outputs the original and transformed names
+      System.out.println("Your name is: " + name); // outputs the original name 
+      System.out.println("If converted to Uppercase it becomes: " + upperCaseName); // outputs the name in uppercase
+      System.out.println("If we move things around a little bit it becomes: " + modifiedName); // outputs the name after its modified
+      
+            }
     
 }
